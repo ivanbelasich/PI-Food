851d0ff30b41e58654a9487e22139ead6b9c5c0d
Ivan Belasich <ivanbelasich@gmail.com>
2022-02-19 17:06:50 -0300

    fix addDiets bug

--- api/src/controllers/diet.js
+++ api/src/controllers/diet.js
@@ -2,8 +2,12 @@ const { Diet } = require("../db");
 
 const addDiets = async (req, res) => {
   try {
-    const dietCreated = await Diet.bulkCreate(req.body);
-    res.send(dietCreated);
+    const findDiet = await Diet.findAll();
+    if (findDiet.length === 0) {
+      const dietCreated = await Diet.bulkCreate(req.body);
+      res.send(dietCreated);
+    }
+    res.send({ message: "Las dietas ya fueron agregadas" });
   } catch {
     res.status(404).json({ message: "Diets could not be added" });
   }
